39422d536916c89631b2d7b43408e6be84ec0d28
Vikas <100078119+VIKAS9793@users.noreply.github.com>
2024-08-05 17:29:46 +0530

    Switched OFF for now

--- device.mk
+++ device.mk
@@ -42,8 +42,5 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
 	$(LOCAL_PATH)
 
-# Swiitchoff stuffs
-$(call inherit-product, swiitchoff/stuffs/config.mk)
-
 # Call the proprietary setup
 $(call inherit-product, vendor/xiaomi/vili/vili-vendor.mk)
